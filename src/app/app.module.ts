d246e6bf55fcadcc0f7b12ca62a304a1a71893b3
Shivam175 <shivamhay175@gmail.com>
2022-09-22 16:10:12 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NgxsModule } from '@ngxs/store';
+import { TutorialState } from './state/tutorial.state';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReadComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,           //Reactive Forms Integration
+    NgxsModule.forRoot([
+      TutorialState
+    ]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+
+})
+export class AppModule { }
